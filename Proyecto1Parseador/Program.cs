2fe9054a5e3e1e4359359d736bb5ef1aa1c9ec14
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-11-15 15:16:53 -0400

    Agregar archivos de proyecto.

---
+++ Proyecto1Parseador/Program.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4.Runtime;
+using Proyecto1Parseador;
+
+Console.WriteLine("Hello, World!");
+
+string input = "5*6+9*3-1";
+Console.WriteLine($"Input {input}\n");
+ICharStream stream = CharStreams.fromString(input);
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+aritmeticaParser  parser = new aritmeticaParser(tokens);
+var tree = parser.aritmetica();
+Aritmetica aritmetica = new Aritmetica();//La clase que creamos
+int resultado= aritmetica.Visit(tree);
